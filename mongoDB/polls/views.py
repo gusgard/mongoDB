f1057923a1062d287b00cdd5d6ebf1eed1794a00
gustavo <gusgard@gmail.com>
2013-12-06 16:46:45 -0200

    borro todo

--- mongoDB/polls/views.py
+++
@@ -1,8 +0,0 @@
-from polls.models import Poll, Choice
-
-poll = Poll.objects(question__contains="What").first()
-choice = Choice(choice_text="I'm at DjangoCon.fi", votes=23)
-poll.choices.append(choice)
-poll.save()
-
-print poll.question
